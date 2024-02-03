d191559d452389f66ce04cae1f5d053db4789f78
jdejesusroa <92193967+jdejesusroa@users.noreply.github.com>
2024-02-03 01:44:56 +0000

    Initial commit

---
+++ libros/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Libro(models.Model):
+  titulo=models.CharField(max_length=200)
+  Autor=models.CharField(max_length=100)
+  resumen=models.TextField()
+
+  def __str__(self):
+    return self.titulo
